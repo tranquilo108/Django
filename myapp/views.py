54fc3df9ea21b3712f07e90c76c67775c4e0ba3a
tranquilo <sanbonzakurakogyoshi@gmail.com>
2024-03-04 23:15:47 +0700

    HW1

--- myapp/views.py
+++ myapp/views.py
@@ -8,4 +8,19 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse('This is a text about us')
+    html = """<!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+        <h1 style="text-align: center">This is a text about us</h1>
+        <p style="text-align: center">My first Django project</p>
+        <p style="text-align: center">And am i</p>
+        </body>
+        </html>"""
+    return HttpResponse(html)
