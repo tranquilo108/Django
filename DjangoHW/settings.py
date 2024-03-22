5b68fa2ba41a1198bdb817165e0d7cdca6815198
tranquilo <sanbonzakurakogyoshi@gmail.com>
2024-03-22 19:09:44 +0700

    Setup requirements.txt and settings.py to deploy

--- DjangoHW/settings.py
+++ DjangoHW/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$vl_7(6cnl5ja2a6xv)gj!htjm-cwxa@7s!2qdon-flmq^n-%'
+# SECRET_KEY = 'django-insecure-$$vl_7(6cnl5ja2a6xv)gj!htjm-cwxa@7s!2qdon-flmq^n-%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.100.9', 'tranquilo.pythonanywhere.com', ]
+INTERNAL_IPS = ['127.0.0.1', ]
 
 # Application definition
 
@@ -74,8 +79,15 @@ WSGI_APPLICATION = 'DjangoHW.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tranquilo$default',
+        'USER': 'tranquilo',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'tranquilo.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -112,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
